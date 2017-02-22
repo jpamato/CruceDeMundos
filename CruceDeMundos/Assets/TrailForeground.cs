8754e6f559e477fef4315dc820697a9facd65615
Dario <superflashilandia@gmail.com>
2017-02-22 20:26:54 -0300

    Bala del antivortex

---
+++ CruceDeMundos/Assets/TrailForeground.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailForeground : MonoBehaviour {
+
+	// Use this for initialization
+ public TrailRenderer trail;
+ // Use this for initialization
+ void Start () {
+     trail.sortingLayerName = "Default";
+     trail.sortingOrder = 2;
+ 
+ }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
