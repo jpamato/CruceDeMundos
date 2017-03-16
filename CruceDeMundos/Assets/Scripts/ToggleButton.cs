c6da3b937bf9b676f0645c31efb41a2d3e252651
jpamato <juanpamato@gmail.com>
2017-03-16 18:26:07 -0300

    fono implement

---
+++ CruceDeMundos/Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleButton : MonoBehaviour {
+
+	public GameObject buttonOn;
+	public GameObject buttonOff;
+
+	// Use this for initialization
+	void Start () {
+		SetButtonOn (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetButtonOn(bool on){
+		buttonOff.SetActive(!on);
+		buttonOn.SetActive(on);
+	}
+}
