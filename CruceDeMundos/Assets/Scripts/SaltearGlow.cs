5abfb4c4742fc394c6ea970ec4f72b4b00bf3a45
jpamato <juanpamato@gmail.com>
2018-06-08 16:54:04 -0300

    btn siguiente late

---
+++ CruceDeMundos/Assets/Scripts/SaltearGlow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaltearGlow : MonoBehaviour {
+
+	public float rango;
+	RectTransform rt;
+
+	float w,h;
+
+	// Use this for initialization
+	void Start () {
+		rt = GetComponent<RectTransform> ();
+		w = rt.sizeDelta.x;
+		h = rt.sizeDelta.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float delta = Game.Instance.globalGlow * rango;
+		rt.sizeDelta = new Vector2 (w+delta, h+delta);
+	}
+}
